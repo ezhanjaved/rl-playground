f1fd1e7fb717d2f840b467e1fb733c0191affd0d
Ezhan Javed <ezhanwrites169@gmail.com>
2026-04-12 16:42:13 +0500

    Update to callWebhook

--- server/utilities/callWebhook.py
+++ server/utilities/callWebhook.py
@@ -6,8 +6,9 @@ import time
 
 import requests
 
-WEBHOOK_URL_TRAINING = str(os.getenv("WEBHOOK_URL_TRAINING"))
-WEBHOOK_URL_INFERENCE = str(os.getenv("WEBHOOK_URL_INFERENCE"))
+WEBHOOK_URL = str(os.getenv("WEBHOOK_URL_TRAINING"))
+TRAINING_WEBHOOK = f"{WEBHOOK_URL}/training-finished"
+INFERENCE_WEBHOOK = f"{WEBHOOK_URL}/model-ready"
 SECRET = os.getenv("WEB_SECRET")
 
 
@@ -18,7 +19,7 @@ def call_webhook_for_training(uid: str, path: str):
     body = json.dumps(payload, separators=(",", ":")).encode()
     signature = hmac.new(SECRET.encode(), body, hashlib.sha256).hexdigest()
     headers = {"Content-Type": "application/json", "x-signature": signature}
-    response = requests.post(WEBHOOK_URL_TRAINING, data=body, headers=headers)
+    response = requests.post(TRAINING_WEBHOOK, data=body, headers=headers)
     if response.status_code != 200:
         raise Exception(f"Webhook failed: {response.status_code} - {response.text}")
     return response.json()
@@ -27,11 +28,15 @@ def call_webhook_for_training(uid: str, path: str):
 def call_webhook_for_inference(uid: str):
     if not SECRET:
         raise ValueError("WEB_SECRET is not set in environment variables")
-    payload = {"model_id": uid, "timestamp": int(time.time())}
+    payload = {
+        "model_id": uid,
+        "timestamp": int(time.time()),
+        "url": "ws//k9co1bxgp1ct5w-8001.proxy.runpod.net",
+    }
     body = json.dumps(payload, separators=(",", ":")).encode()
     signature = hmac.new(SECRET.encode(), body, hashlib.sha256).hexdigest()
     headers = {"Content-Type": "application/json", "x-signature": signature}
-    response = requests.post(WEBHOOK_URL_INFERENCE, data=body, headers=headers)
+    response = requests.post(INFERENCE_WEBHOOK, data=body, headers=headers)
     if response.status_code != 200:
         raise Exception(f"Webhook failed: {response.status_code} - {response.text}")
     return response.json()
