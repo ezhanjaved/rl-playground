2dc4e86ef3eb2ffc6946b4ddde24dfe11613e36b
Ezhan Javed <ezhanwrites169@gmail.com>
2026-04-13 02:19:06 +0500

    Update path.config

--- server/path_config.py
+++ server/path_config.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent  # this resolves to /server
+BASE_DIR = Path("/workspace/rl-playground/server")
 DATA_DIR = BASE_DIR / "data"
 MODEL_DIR = BASE_DIR / "training" / "trainer" / "models"
