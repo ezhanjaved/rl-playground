aba6fffab5a256104207b5e75d07437a7fb9ee04
Ezhan Javed <ezhanwrites169@gmail.com>
2026-04-13 02:54:55 +0500

    Update

--- server/training/wrappers/gymWrapper.py
+++ server/training/wrappers/gymWrapper.py
@@ -1,52 +1,73 @@
+import gymnasium as gym
 import numpy as np
 from gymnasium import spaces
 
 from server.utilities.refined import actionTrasnlator, refined, simplify
 
 
-class GymWrapper:
+class GymWrapper(gym.Env):
     def __init__(self, engine):
+        super().__init__()
+
         self.engine = engine
+
         obs_dict = self.engine.reset()
         if len(obs_dict) != 1:
             raise ValueError("GymWrapper only supports single-agent environments.")
+
         obs_list, agent_id = simplify(obs_dict)
         self.agent_id = agent_id
-        _, shape = refined(obs_list)
+
+        refined_obs, shape = refined(obs_list)
         self.obs_shape = shape
 
-    def reset(self):
+        self.observation_space = spaces.Box(
+            low=-np.inf,
+            high=np.inf,
+            shape=(self.obs_shape,),
+            dtype=np.float32,
+        )
+
+        self.action_space = spaces.Discrete(9)
+
+    def reset(self, *, seed=None, options=None):
+        super().reset(seed=seed)
+
         obs_dict = self.engine.reset()
+
         if len(obs_dict) != 1:
             raise ValueError("GymWrapper only supports single-agent environments.")
+
         obs_list, agent_id = simplify(obs_dict)
+
         if agent_id != self.agent_id:
             raise ValueError("Agent ID changed between resets.")
+
         refined_obs, _ = refined(obs_list)
         refined_obs = np.array(refined_obs, dtype=np.float32)
+
         return refined_obs, {}
 
     def step(self, action):
         action_str = actionTrasnlator(action)
         actionDict = {self.agent_id: action_str}
+
         obs_dict, rew_dict, ter_dict, tru_dict, info_dict = self.engine.step(actionDict)
+
         if len(obs_dict) != 1:
             raise ValueError("GymWrapper only supports single-agent environments.")
+
         obs_list, agent_id = simplify(obs_dict)
+
         if agent_id != self.agent_id:
             raise ValueError("Agent ID changed between resets.")
+
         refined_obs, _ = refined(obs_list)
         refined_obs = np.array(refined_obs, dtype=np.float32)
+
         reward = rew_dict[self.agent_id]
         terminated = ter_dict[self.agent_id]
         truncated = tru_dict[self.agent_id]
         info = info_dict[self.agent_id]
+
         return refined_obs, reward, terminated, truncated, info
-
-    def observation_space(self):
-        self.observation_space = spaces.Box(
-            low=np.inf, high=np.inf, shape=(self.obs_shape), dtype=np.float32
-        )
-
-    def action_space(self):
-        self.action_space = spaces.Discrete(9)
