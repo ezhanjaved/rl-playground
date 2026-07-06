93c481a6eb39e5e8556a11a9f9583333f662055a
Ezhan Javed <ezhanwrites169@gmail.com>
2026-07-07 03:02:36 +0500

    Update to bulletWorld

--- server/training/bulletWorld.py
+++ server/training/bulletWorld.py
@@ -1,7 +1,7 @@
 import math
 import random
-import time
 
+# import time
 import pybullet as p
 import pybullet_data
 
@@ -28,7 +28,7 @@ class PyBulletWorld:
                 p.disconnect(self.client)
             except Exception:
                 pass
-        self.client = p.connect(p.GUI)
+        self.client = p.connect(p.DIRECT)
         self.entity_mapping = {}
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
         p.setGravity(0, 0, -9.81, physicsClientId=self.client)
@@ -416,7 +416,7 @@ class PyBulletWorld:
     def step_simulation(self, steps=1):
         for _ in range(steps):
             p.stepSimulation(physicsClientId=self.client)
-            time.sleep(1 / 60)
+            # time.sleep(1 / 60)
 
     def settle(self, steps=2):
         for _ in range(steps):
